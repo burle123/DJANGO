5154330cdd2330aba36fb39de4dd4b094127cb33
burle123 <burleshantanu@gmail.com>
2025-09-09 16:51:18 +0530

    Table Creation and Display on Web Page

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    position = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='employee_photos', null=True, blank=True)
+    mobile_number = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    hire_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.position}"
--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from employees.models import Employee
+
 def home(request):
     # return HttpResponse("<h2>Hello, welcome to the home page!</h2>")    # Simple HTTP response
-    
-    return render(request, 'homme.html')  # Render the template
+    employees = Employee.objects.all()  # Fetch all employee records  
+    # print(employees)  # Debugging: Print to console  
+    context={
+        'employees': employees
+    }
+    return render(request, 'homme.html' , context)  # Render the template
+
+ 
