373816e2d93b9b11e71b98f3d76682489e5d2afe
burle123 <burleshantanu@gmail.com>
2025-09-11 12:30:56 +0530

    Fetching Employee Deatils from ! page to another page

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,19 @@
+from django.shortcuts import get_object_or_404 
 from django.shortcuts import render
+from .models import Employee
+
+# from django.http import HttpResponse
+
 
 # Create your views here.
+
+def employee_detail(request, id):
+    employee = get_object_or_404(Employee, id=id)
+    context={
+        "employee":employee,
+    }
+    return render(request ,'employee_detail.html',context)
+    
+    
+   
+
